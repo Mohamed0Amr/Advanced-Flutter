50f78b97e46d2e8ff8d61636d91b5dc4834ed333
Mohamed0Amr <140004433+Mohamed0Amr@users.noreply.github.com>
2025-07-08 10:55:38 +0300

    Update many

--- lib/features/onboarding/onboarding_screen.dart
+++ lib/features/onboarding/onboarding_screen.dart
@@ -1,10 +1,26 @@
+import 'package:cubit/features/onboarding/widgets/doc_logo_and_name.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class OnBoardingScreen extends StatelessWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("OnBoarding Screen")));
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(padding: EdgeInsets.only(
+            top: 30.h,
+            bottom: 30.h
+          ),
+          child: const Column(
+            children: [
+              DocLogoAndName(),
+            ],
+          ),
+          ),
+        ),
+      ));
   }
 }
